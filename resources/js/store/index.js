6edf336e6d09680b63752c42fb595ee1fb61e30d
Saldi Sucipto <saldisucipto@gmail.com>
2021-12-28 11:28:57 +0700

    Just Try

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -11,10 +11,14 @@ export default createStore({
     },
     mutations: {
         // Mutations berguna untuk mengubah setiap state yang berubah
+        // Mutations untuk melakukan dan merubah data pada state yang ada
         setUserData(state, userData) {
             (state.user = userData),
                 localStorage.setItem("user", JSON.stringify(userData));
             axios.defaults.headers.common.Authorization = `Bearer ${userData.token}`;
+        },
+        setCurentUser(state, value) {
+            state.currentsUser = value;
         },
         clearUserData() {
             localStorage.removeItem("user");
@@ -22,11 +26,14 @@ export default createStore({
         },
     },
     actions: {
-        // Melakukan Perubahan pada State
+        // Melakukan Perubahan pada State atau Logic
         async login({ commit }, credentials) {
             // console.log(credentials);
+            // commit berfungsi untuk men trigger perubahan yang dilakukan mutations
             return axios.post("/login", credentials).then(({ data }) => {
+                console.log(data);
                 commit("setUserData", data);
+                commit("setCurentUser", data);
             });
         },
 
@@ -35,6 +42,8 @@ export default createStore({
         },
     },
     getters: {
+        // Mengembalikan data terbaru yang ada pada state sesuai perubahan state
         isLogged: (state) => !!state.user,
+        currentsUser: (state) => state.currentsUser,
     },
 });
